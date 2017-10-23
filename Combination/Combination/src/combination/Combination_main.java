7f17866f038baa24c70be052d81f6df09ceb9417
Hidesato Ikeya <iky.hdst@gmail.com>
2017-10-23 18:56:48 +0900

    commit 3

--- Combination/Combination/src/combination/Combination_main.java
+++ Combination/Combination/src/combination/Combination_main.java
@@ -9,9 +9,13 @@ public class Combination_main {
 		Combination_lib clib = new Combination_lib();
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Input a number >");
+		System.out.println("Input a number s>");
 		int s = Integer.parseInt(scan.next());
-		System.out.println(s + "! = " + clib.getFactorial(s));
+		System.out.println("Input a number r>");
+		int r = Integer.parseInt(scan.next());
+
+		System.out.println(s + "C" + r + " = "
+			+ clib.getFactorial(s) / (clib.getFactorial(r)*clib.getFactorial(s-r)));
 	}
 
 }
