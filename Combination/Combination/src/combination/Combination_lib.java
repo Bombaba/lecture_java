bafb50b97e3244ed702a7a662ca787517aa19f06
Hidesato Ikeya <iky.hdst@gmail.com>
2017-10-23 18:52:55 +0900

    commit2

--- Combination/Combination/src/combination/Combination_lib.java
+++ Combination/Combination/src/combination/Combination_lib.java
@@ -1,5 +1,12 @@
 package combination;
 
 public class Combination_lib {
-
+	public int getFactorial(int n) {
+		if (n <= 1) {
+			return 1;
+		}
+		else {
+			return n * getFactorial(n - 1);
+		}
+	}
 }
--- Combination/Combination/src/combination/Combination_main.java
+++ Combination/Combination/src/combination/Combination_main.java
@@ -1,10 +1,17 @@
 package combination;
 
+import java.util.Scanner;
+
 public class Combination_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Combination_lib clib = new Combination_lib();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Input a number >");
+		int s = Integer.parseInt(scan.next());
+		System.out.println(s + "! = " + clib.getFactorial(s));
 	}
 
 }
